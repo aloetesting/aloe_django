16d204d0d7685fdc6770e63d0628cfe9184e8192
Danielle Madeley <dmadeley@infoxchange.net.au>
2015-08-07 20:32:45 +1000

    Fix Sphinx decl

--- aloe_django/mail/backends.py
+++ aloe_django/mail/backends.py
@@ -24,7 +24,7 @@ from django.core.mail.backends.base import BaseEmailBackend
 class QueueEmailBackend(BaseEmailBackend):
     """
     Email backend that allows mail to be captured by tests run between
-    separate processes via :module:`multiprocessing`.
+    separate processes via :mod:`multiprocessing`.
     """
 
     def send_messages(self, messages):
