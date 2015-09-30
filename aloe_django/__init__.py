70da0a5cd37a705b07583cb397fdf5dfa25d8601
Alexey Kotlyarov <a@koterpillar.com>
2015-09-30 19:07:50 +1000

    Silence spurious Pylint warnings

--- aloe_django/__init__.py
+++ aloe_django/__init__.py
@@ -11,13 +11,17 @@ try:
     # In Django 1.8, transparently serving static files was moved from
     # LiveServerTestCase to StaticLiveServerTestCase
     try:
+        # pylint:disable=no-name-in-module
         from django.contrib.staticfiles.testing import (
             StaticLiveServerTestCase as LiveServerTestCase)
+        # pylint:enable=no-name-in-module
     except ImportError:
         from django.test import LiveServerTestCase
 
     from aloe.testclass import TestCase as AloeTestCase
 
+    # pylint:disable=abstract-method
+    # Pylint cannot infer methods dynamically added by Aloe
     class TestCase(LiveServerTestCase, AloeTestCase):
         """
         Base test class for Django Gherkin tests.
