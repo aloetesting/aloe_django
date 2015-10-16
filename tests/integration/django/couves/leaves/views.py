6faf146e92e373be6bacc712b525ddcb58d028f9
Alexey Kotlyarov <a@koterpillar.com>
2015-10-16 21:36:17 +1100

    Remove obsolete tests
    The tests described incorrect behavior, but were accidentally succeeding
    thanks to passing wrong arguments to harvest.

--- tests/integration/django/couves/leaves/views.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('OK')
--- tests/integration/django/couves/urls.py
+++
@@ -1,9 +0,0 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:
-    from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns(
-    '',
-    url(r'', 'couves.leaves.views.index'),
-)
